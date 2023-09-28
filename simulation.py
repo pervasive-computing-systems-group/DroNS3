44a93f3a3ec4e4a5232ce9a0295dc2bb7c2cad31
pecs_Desktop <minespecs2022@gmail.com>
2023-09-28 12:42:45 -0600

    changing file path

--- simulation.py
+++ simulation.py
@@ -34,5 +34,5 @@ class Custom_Command(commands.Command):
 
 if __name__ == '__main__':
 	w = Wrapper()
-	w.start_mission(mission=missions.General(w.vehicle, mission_file="mission.pln", debug = True, custom_commands=[Custom_Command], is_sim=True))
+	w.start_mission(mission=missions.General(w.vehicle, mission_file="sample_wsn_mission/drone_plan.pln", debug = True, custom_commands=[Custom_Command], is_sim=True))
 	
