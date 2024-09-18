0d9d0d37ca8c3e6ac6cb3e96829631bd0444958f
DesaulniersM <mdesaulniersm@gmail.com>
2024-09-18 15:08:54 -0600

    forgot a

--- fly.py
+++ fly.py
@@ -17,7 +17,7 @@ from mission_wrapper import Wrapper
 
 if __name__ == '__main__':
 	w = Wrapper(is_sim=False)
-	w.start_mission(mission=missions.General(w.vehicle, mission_file="sample_wsn_mission.flight_test.pln", debug = True, is_sim=True))
+	w.start_mission(mission=missions.General(w.vehicle, mission_file="sample_wsn_mission/flight_test.pln", debug = True, is_sim=True))
 	# w.start_mission(mission=missions.ConnectionTests())
 	#w.start_mission(mission=missions.CollectWSNData(plan_path="sample_wsn_mission/drone_plan.pln", node_path = "sample_wsn_mission/node_info.txt", output_path = "./", algorithm = "DEFAULT"))
 	
