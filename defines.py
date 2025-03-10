e7516b19d93ece89ea777a8baca127a22b66ecb5
pecs_Desktop <minespecs2022@gmail.com>
2025-03-10 11:52:51 -0600

    Using single folder design

--- defines.py
+++ defines.py
@@ -1,20 +1,21 @@
 #Path to NS_3 executable from the root directory (i.e. "/home/...")
 NS_3_PATH = "/home/minespecs/NS3/ns-allinone-3.36.1/ns-3.36.1/ns3"
 
-#Path to SimpleNetSim executable from the root directory (i.e. "/home/...")
-SNS_PATH = "/home/jonathan/Research/HolisticFramework/DroNS3/SimpleNetSim/Simulation/sim"
-# Path to LocalPlanner from the root directory (i.e. "/home/...")
-LOCAL_PLANNER_PATH = "/home/jonathan/Research/HolisticFramework/MissionPlanner/build/local-planner"
+## Holistic planner paths
 # Path to the orchestrator director (i.e. "/home/...")
-ORCHESTRATOR_PATH = "/home/jonathan/Research/HolisticFramework/Orchestrator/"
+ORCHESTRATOR_PATH = "/home/minespecs/Research/HolisticFramework/"
+# Path to SimpleNetSim executable from the root directory (i.e. "/home/...")
+SNS_PATH = ORCHESTRATOR_PATH+"DroNS3/SimpleNetSim/Simulation/sim"
+# Path to LocalPlanner from the root directory (i.e. "/home/...")
+LOCAL_PLANNER_PATH = ORCHESTRATOR_PATH+"MissionPlanner/build/local-planner"
 
-#Path to the mission directory from the root directory (i.e. "/home/...")
+# Path to the mission directory from the root directory (i.e. "/home/...")
 MISSION_PATH = "/home/minespecs/RDS/sample_wsn_mission/"
-#Path to the LKH executable from the root directory
+# Path to the LKH executable from the root directory
 LKH_PATH = "/home/minespecs/Documents/LKH-3.0.8/LKH"
-#IP Address of current machine
+# IP Address of current machine
 IP_ADDRESS = "138.67.20.164"
-#Enables debug prints
+# Enables debug prints
 debug = True
-#Communications Protocol PAth
+# Communications Protocol PAth
 Comms_Path = None
